40f484aed2470239f7410d329e0e204326d52d35
Benbob02 <104645237+BenjaminBJohnson@users.noreply.github.com>
2022-05-04 19:51:04 -0400

    week 3 start

--- week03/assignment/setup_files_dirs.py
+++ week03/assignment/setup_files_dirs.py
@@ -13,9 +13,9 @@ def create_dir(folder):
 def create_images(video_file, folder):
     # limit the number of frames to 300
     if platform.system() == 'Windows':
-        command = rf'ffmpeg -i {video_file} -vframes 300 {folder}/image%3d.png'
+        command = f'ffmpeg -i {video_file} -vframes 300 {folder}/image%3d.png'
     else:
-        command = rf'./ffmpeg -i {video_file} -vframes 300 {folder}/image%3d.png'
+        command = f'./ffmpeg -i {video_file} -vframes 300 {folder}/image%3d.png'
     os.system(command)
 
 def main():
